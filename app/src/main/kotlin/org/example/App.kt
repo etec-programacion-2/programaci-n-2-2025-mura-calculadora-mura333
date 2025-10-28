3cf5ce7c7cbdde2b63fd07c29543450f95e4518f
Evaraschin <e.varaschin@alumno.etec.um.edu.ar>
2025-10-28 12:12:19 -0300

    Crear un Parser para Expresiones en Notacion Polaca Inversa

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -4,7 +4,7 @@ fun main() {
     val calc = Calculadora()
     val operaciones = listOf("5 3 +", "10 4 -", "6 7 *", "15 3 /",
         "30 sen", "60 cos", "45 tan", "2 3 pow", "16 raiz",
-        "3 4 5 * +", "7 2 3 * -", "5 0 /")
+        "3 4 5 * +", "7 2 3 * -", "4 2 /")
 
     operaciones.forEach { try { println("$it = ${calc.calcular(it)}") }
     catch (e: Exception) { println("$it -> ${e.message}") } }
